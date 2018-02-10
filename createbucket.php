628ee70f50d198ca66e6dcd3b0fd66e2d8ef30c9
awscloudtraining <36339916+awscloudtraining@users.noreply.github.com>
2018-02-11 00:43:06 +0530

    Updated

--- createbucket.php
+++ createbucket.php
@@ -1,11 +1,10 @@
 <?php
-//copyright 2015 - A Cloud Guru.
 
 //connection string
 include 'connecttoaws.php';
 
 // Create a unique bucket name
-$bucket = uniqid("acloudguru", true);
+$bucket = uniqid("awscloudtraining", true);
 
 // Create our bucket using our unique bucket name
 $result = $client->createBucket(array(
@@ -13,8 +12,8 @@ $result = $client->createBucket(array(
 ));
 
 //HTML to Create our webpage
-echo "<h1 align=\"center\">Hello Cloud Guru!</h1>";
-echo "<div align = \"center\"><img src=\"https://acloud.guru/images/logo-small-optimised.png\"></img></div>";
+echo "<h1 align=\"center\">Hello and Welcome to AWS Cloud Training!</h1>";
+echo "<div align = \"center\"><img src=\"https://s3.ap-south-1.amazonaws.com/vijaytest1234/Capture.JPG"></img></div>";
 echo "<h2 align=\"center\">You have successfully created a bucket called {$bucket}</h2>";
 echo "<div align=\"center\"><a href=\"createfile.php?bucket=$bucket\">Click Here to Continue</a></div>";
 ?>
